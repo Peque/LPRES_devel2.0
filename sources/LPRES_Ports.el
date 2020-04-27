13dcc9edfd76c50ca5b627a5d7bb4dca65b96938
Luis Sánchez de León Peque <luis.slpeque@gmail.com>
2020-04-27 15:26:32 +0200

    updated LSdLP --- turbomachinery components reviewed
    Signed-off-by: Luis Sánchez de León Peque <luis.slpeque@gmail.com>

--- sources/LPRES_Ports.el
+++ sources/LPRES_Ports.el
@@ -1,14 +1,15 @@
 --------------------------------------------------------------------------------
 -- EcosimPro Simulation Source Code
 -- FILE NAME: LPRES_Ports.el
--- DESCRIPTION: Defines common port types
--- NOTES:
--- AUTHOR: Pablo Sierra Heras
+-- DESCRIPTION: Defines all necessary port types
+-- NOTES: -- updated LSdLP 26/04/2020
+-- AUTHOR: Pablo Sierra Heras & Juan Velasco Mariscal
 -- CREATION DATE: 05/12/2014
+-- LAST MODIFIED: 26/04/2020
 --------------------------------------------------------------------------------
 
 -- Libraries
-USE MATH VERSION "3.1"
+USE MATH VERSION "3.2"   -- updated LSdLP 26/04/2020
 
 
 -- Ports
@@ -17,13 +18,13 @@ USE MATH VERSION "3.1"
 --------------------------------------------------------------------------------
 PORT Fluid SINGLE IN
 
-	"Fluid port"
+   "Fluid port"
 
-	EQUAL 			REAL Tt						UNITS u_K			RANGE ZERO,Inf		"Total temperature" 
-	EQUAL 			REAL pt						UNITS u_Pa			RANGE ZERO,Inf		"Total pressure"
-	SUM				REAL W						UNITS u_kg_s		RANGE ZERO,Inf		"Mass flow"
-	HIDDEN EQUAL	REAL fluid[ChemName]	UNITS no_units		RANGE ZERO,Inf		"Working fluid"
-	
+   EQUAL             REAL Tt                 UNITS u_K            RANGE ZERO,Inf       "Total temperature"
+   EQUAL             REAL pt                 UNITS u_Pa           RANGE ZERO,Inf       "Total pressure"
+   SUM               REAL W                  UNITS u_kg_s         RANGE ZERO,Inf       "Mass flow"
+   HIDDEN EQUAL      REAL fluid[ChemName]    UNITS no_units       RANGE ZERO,Inf       "Working fluid"
+
 END PORT
 
 --------------------------------------------------------------------------------
@@ -31,14 +32,14 @@ END PORT
 --------------------------------------------------------------------------------
 PORT FluidInj SINGLE
 
-	"Fluid injection port"
+   "Fluid injection port"
 
-	EQUAL 			REAL T						UNITS u_K			RANGE ZERO,Inf		"Temperature" 
-	EQUAL 			REAL p						UNITS u_Pa			RANGE ZERO,Inf		"Pressure"
-	SUM				REAL W						UNITS u_kg_s		RANGE ZERO,Inf		"Mass flow"
-	HIDDEN EQUAL 	REAL fluid[ChemName]		UNITS no_units		RANGE ZERO,Inf		"Working fluid"
-	EQUAL 			REAL p_c						UNITS u_Pa			RANGE ZERO,Inf		"Chamber pressure"
-	
+   EQUAL             REAL T                  UNITS u_K            RANGE ZERO,Inf       "Temperature"
+   EQUAL             REAL p                  UNITS u_Pa           RANGE ZERO,Inf       "Pressure"
+   SUM               REAL W                  UNITS u_kg_s         RANGE ZERO,Inf       "Mass flow"
+   HIDDEN EQUAL      REAL fluid[ChemName]    UNITS no_units       RANGE ZERO,Inf       "Working fluid"
+   EQUAL             REAL p_c                UNITS u_Pa           RANGE ZERO,Inf       "Chamber pressure"
+
 END PORT
 
 --------------------------------------------------------------------------------
@@ -46,16 +47,16 @@ END PORT
 --------------------------------------------------------------------------------
 PORT GasNozzle SINGLE
 
-	 "Gas port through a nozzle"
+   "Gas port through a nozzle"
 
-	EQUAL 			REAL Tt						UNITS u_K			RANGE ZERO,Inf		"Total temperature" 
-	EQUAL 			REAL pt						UNITS u_Pa			RANGE ZERO,Inf		"Total pressure"
-	SUM				REAL W						UNITS u_kg_s		RANGE ZERO,Inf		"Mass flow"
-	HIDDEN EQUAL	REAL fluid[ChemName]		UNITS no_units		RANGE ZERO,Inf		"Working fluid"
-	EQUAL 			REAL A_out					UNITS u_m2			RANGE ZERO,Inf		"Nozzle output area"
-	EQUAL 			REAL c_star					UNITS u_m_s			RANGE ZERO,Inf		"Characteristic velocity"
-	EQUAL				REAL A_th					UNITS u_m2			RANGE ZERO,Inf		"Throat area"
-		
+   EQUAL             REAL Tt                 UNITS u_K            RANGE ZERO,Inf       "Total temperature"
+   EQUAL             REAL pt                 UNITS u_Pa           RANGE ZERO,Inf       "Total pressure"
+   SUM               REAL W                  UNITS u_kg_s         RANGE ZERO,Inf       "Mass flow"
+   HIDDEN EQUAL      REAL fluid[ChemName]    UNITS no_units       RANGE ZERO,Inf       "Working fluid"
+   EQUAL             REAL A_out              UNITS u_m2           RANGE ZERO,Inf       "Nozzle output area"
+   EQUAL             REAL c_star             UNITS u_m_s          RANGE ZERO,Inf       "Characteristic velocity"
+   EQUAL             REAL A_th               UNITS u_m2           RANGE ZERO,Inf       "Throat area"
+
 END PORT
 
 --------------------------------------------------------------------------------
@@ -63,10 +64,10 @@ END PORT
 --------------------------------------------------------------------------------
 PORT Mechanical
 
-	"Mechanical port"
-	
-   SUM   REAL Power 		UNITS u_W										"Mechanical power"
-   EQUAL REAL N			UNITS u_rad_s		RANGE ZERO,Inf			"Rotational speed"
+   "Mechanical port"
+
+   SUM               REAL Power              UNITS u_W                                 "Mechanical power"
+   EQUAL             REAL N                  UNITS u_rad_s        RANGE ZERO,Inf       "Rotational speed"
 
 END PORT
 
@@ -75,12 +76,12 @@ END PORT
 --------------------------------------------------------------------------------
 PORT Heat SINGLE IN
 
-	"Heat exchange port"
-	
-	SUM	REAL Q		UNITS u_W 									"Heat flux"
-	EQUAL REAL T		UNITS u_K			RANGE ZERO,Inf		"Temperature" 
-	EQUAL	REAL A		UNITS u_m2			RANGE ZERO,Inf		"Contact area"
-	
+   "Heat exchange port"
+
+   SUM               REAL Q                  UNITS u_W                                 "Heat flux"
+   EQUAL             REAL T                  UNITS u_K            RANGE ZERO,Inf       "Temperature"
+   EQUAL             REAL A                  UNITS u_m2           RANGE ZERO,Inf       "Contact area"
+
 END PORT
 
 --------------------------------------------------------------------------------
@@ -88,21 +89,21 @@ END PORT
 --------------------------------------------------------------------------------
 PORT Heat_2 SINGLE IN
 
-	"Heat exchange port"
-	
-	SUM	REAL Q		UNITS u_W 									"Heat flux"
-	EQUAL REAL T		UNITS u_K			RANGE ZERO,Inf		"Temperature" 
-	EQUAL	REAL A		UNITS u_m2			RANGE ZERO,Inf		"Contact area"
-	
+   "Heat exchange port"
+
+   SUM               REAL Q                  UNITS u_W                                 "Heat flux"
+   EQUAL             REAL T                  UNITS u_K            RANGE ZERO,Inf       "Temperature"
+   EQUAL             REAL A                  UNITS u_m2           RANGE ZERO,Inf       "Contact area"
+
 END PORT
-	
+
 --------------------------------------------------------------------------------
 -- Port for information exchanges between components
 --------------------------------------------------------------------------------
 PORT Info (INTEGER n)
 
-	"Information exchange port"
-	
-	SUM REAL Data[n]			UNITS no_units 			"Information array"
-	
+   "Information exchange port"
+
+   SUM               REAL Data[n]            UNITS no_units                            "Information array"
+
 END PORT
--- sources/version.el
+++ sources/version.el
@@ -1,10 +1,9 @@
 -- Define the version, date, author and comments associated to this library
 #LIBRARY_INFO
-  #VERSION = "1.3.5"
-  #DATE = "13/06/2017"
-  #AUTHOR= "Pablo Sierra Heras" 
-  #COMMENTS= "No comments"
-  					-- Some mistakes corrected.
-					-- Component Turbine_liq created.
-					-- Turbomachinery models improved.
+  #VERSION = "2.0.1"
+  #DATE = "26/04/2020"
+  #AUTHOR= "UPM"
+  #COMMENTS= "updated LSdLP --- turbomachinery components reviewed"
+               -- Juan Velasco Mariscal   - v2.0      - 15/07/2019
+               -- Pablo Sierra Heras      - v1.3.5    - 13/06/2017
 END #LIBRARY_INFO
